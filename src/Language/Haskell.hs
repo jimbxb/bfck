21f478a7d928828a7ccc1b5d0eb075a5bf3a36bd
James Barnes <jamesbarnes2505@gmail.com>
2022-12-30 15:21:40 +1100

    cleaned up Haskell transpiler

--- src/Language/Haskell.hs
+++ src/Language/Haskell.hs
@@ -10,12 +10,30 @@ transpile _ program = do
     unlines $
     [ "import Data.Char"
     , ""
-    , "data Tape = Tape { left :: [Int], current :: Int, right :: [Int] }"
+    , "data Tape = Tape { left' :: [Int], current :: Int, right' :: [Int] }"
     , ""
-    , "iterateUntilM :: (Monad m) => (a -> Bool) -> (a -> m a) -> a -> m a"
-    , "iterateUntilM p f v"
-    , "  | p v       = return v"
-    , "  | otherwise = f v >>= iterateUntilM p f"
+    , "incr :: Tape -> IO Tape"
+    , "incr (Tape l c r) = return (Tape l (c + 1) r)"
+    , ""
+    , "decr :: Tape -> IO Tape"
+    , "decr (Tape l c r) = return (Tape l (c - 1) r)"
+    , ""
+    , "left :: Tape -> IO Tape"
+    , "left (Tape l c (r:r')) = return (Tape (c:l) r r')"
+    , ""
+    , "right :: Tape -> IO Tape"
+    , "right (Tape (l:l') c r) = return (Tape l' l (c:r))"
+    , ""
+    , "input :: Tape -> IO Tape"
+    , "input (Tape l _ r) = getChar >>= \\c -> return (Tape l (ord c) r)"
+    , ""
+    , "output :: Tape -> IO Tape"
+    , "output t@(Tape _ c _) = putStr [chr (fix c)] >> return t"
+    , ""
+    , "loop :: (Tape -> IO Tape) -> Tape -> IO Tape"
+    , "loop _ t@(Tape _ c _) | fix c == 0 = return t"
+    , "loop f t = f t >>= loop f"
+    , "{-# INLINE loop #-}"
     , ""
     , "fix :: Int -> Int"
     , "fix x = mod (mod x 256 + 256) 256"
@@ -30,13 +48,11 @@ transpile _ program = do
     concatMap (map (uncurry indent) . go 1) program ++ ["  >> return ()"]
   where
     indent i s = replicate (2 * i) ' ' ++ ">>= " ++ s
-    go i Incr = [(i, "\\(Tape l c r) -> return (Tape l (c + 1) r)")]
-    go i Decr = [(i, "\\(Tape l c r) -> return (Tape l (c - 1) r)")]
-    go i Left' = [(i, "\\(Tape l c (r:r')) -> return (Tape (c:l) r r')")]
-    go i Right' = [(i, "\\(Tape (l:l') c r) -> return (Tape l' l (c:r))")]
-    go i Input =
-      [(i, "\\(Tape l _ r) -> getChar >>= \\c -> return (Tape l (ord c) r)")]
-    go i Output = [(i, "\\t@(Tape _ c _) -> putStr [chr (fix c)] >> return t")]
+    go i Incr = [(i, "incr")]
+    go i Decr = [(i, "decr")]
+    go i Left' = [(i, "left")]
+    go i Right' = [(i, "right")]
+    go i Input = [(i, "input")]
+    go i Output = [(i, "output")]
     go i (Loop loop) =
-      [(i, "iterateUntilM (\\(Tape _ c _) -> fix c == 0) (\\t -> return t")] ++
-      concatMap (go $ i + 1) loop ++ [(i + 1, "return)")]
+      [(i, "loop (\\t -> return t")] ++ concatMap (go $ i + 1) loop ++ [(i + 1, "return)")]
